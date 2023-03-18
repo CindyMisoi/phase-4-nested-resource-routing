a4a4929c868bd3f8df2b6f83a16fbc638af7254e
CindyMisoi <crmisoi2k@gmail.com>
2023-03-19 00:10:51 +0300

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
-  resources :dog_houses, only: [:show]
+
+  resources :dog_houses, only: [:show] do 
+    # nested resources => child
+    resources :reviews, only: [:index, :show]
+  end
+
   resources :reviews, only: [:show, :index, :create]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  
+  # # gets all reviews for a specific dog_house
+  # get '/dog_houses/:dog_house_id/reviews', to: 'dog_houses#reviews_index'
+
+  # # get a specific review for a specific dog_house
+  # get '/dog_houses/:dog_house_id/reviews/:id', to: 'dog_houses#review'
+
 end
